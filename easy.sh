aead5034aee136c8675c9ad0023893f40d364623
lijiajun1018 <lijiajun3029@gmail.com>
2024-09-27 19:16:45 +0800

    update easy.sh

--- easy.sh
+++ easy.sh
@@ -5,15 +5,16 @@ moon add some_package # from moon.mod.json
 
 git submodule update --init --recursive
 
-# local test
-moon test
+# local compiler test
+moon test # 扫描整个模块，识别并运行所有内联测试以及以_test.mbt结尾的文件
 moon test -v
 moon run src/bin/main.mbt some_file.mbt
-# moon run src/bin/main.mbt src/utils.mbt
 # moon run src/bin/main.mbt test/test_src/ack.mbt
 
-# dev
-
-
-# remote test
+# remote compiler test
 git archive -o submit.zip HEAD
+
+# local language debug
+ moon run test_language/main.mbt
+
+ # dev
